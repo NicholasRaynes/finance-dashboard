7b94cea55e99a24ac36317c732aff51c2a5bff67
NicholasRaynes <nicholasraynesmb@gmail.com>
2024-09-18 20:23:57 -0500

    Implemented BrowserRouter for App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className='app'>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </BrowserRouter>
   </div>;
 }
 
