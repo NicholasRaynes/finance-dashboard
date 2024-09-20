91c97c99b1d169d3b2f85d2b1fad6ed9be0b7ff4
NicholasRaynes <nicholasraynesmb@gmail.com>
2024-09-19 20:13:55 -0500

    Setup initial routes with placeholders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -10,6 +10,12 @@ function App() {
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Box width="100%" height="10%" padding="1rem 2rem 4rem 2rem">
+          <Routes>
+            <Route path="/" element={<div>dashboard page</div>} />
+            <Route path="/predictions" element={<div>predictions page</div>} />
+          </Routes>
+        </Box>
       </ThemeProvider>
     </BrowserRouter>
   </div>;
