3fc6b3ab9af244ab2b22930fa320bd23da263e60
NicholasRaynes <nicholasraynesmb@gmail.com>
2024-09-20 20:51:44 -0500

    Basic navigation bar structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -11,6 +12,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box width="100%" height="10%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
           <Routes>
             <Route path="/" element={<div>dashboard page</div>} />
             <Route path="/predictions" element={<div>predictions page</div>} />
